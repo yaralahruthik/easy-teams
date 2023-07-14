1b5aa4460bd91b8ac2f0b200762306fe51a0f226
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-07-14 20:00:11 +0100

    Spinner Skeleton

---
+++ src/lib/components/Spinner.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { Settings } from '$lib/types';
+
+	export let settings: Settings;
+</script>
+
+<div>
+	<p>Spinner</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
 	import Header from '$lib/components/Header.svelte';
+	import Spinner from '$lib/components/Spinner.svelte';
 	import type { Settings } from '$lib/types';
 
 	let settings: Settings = {
 		numOfTeams: 2,
-		players: [],
+		players: [
+			{ id: 1, name: 'One' },
+			{ id: 2, name: 'Two' },
+			{ id: 3, name: 'Three' },
+			{ id: 4, name: 'Four' }
+		],
 		teams: [],
 		quickMode: false
 	};
 </script>
 
 <Header bind:settings />
+<Spinner bind:settings />
